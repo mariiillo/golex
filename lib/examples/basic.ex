51dc39b2b6f302c2493179d3f09662e7259ca464
Mario Viapiano <mario.viapiano@wertfaktor.de>
2022-04-02 13:09:59 +0200

    Add basic example data

---
+++ lib/examples/basic.ex
@@ -0,0 +1,29 @@
+defmodule Golex.Examples.Basic do
+  alias Golex.Core.{World, Cell}
+
+  def state do
+    %{
+      {0, 0} => %Cell{status: 'alive'},
+      {0, 1} => %Cell{status: 'dead'},
+      {0, 2} => %Cell{status: 'dead'},
+      {0, 3} => %Cell{status: 'dead'},
+      {1, 0} => %Cell{status: 'alive'},
+      {1, 1} => %Cell{status: 'alive'},
+      {1, 2} => %Cell{status: 'dead'},
+      {1, 3} => %Cell{status: 'alive'},
+      {2, 0} => %Cell{status: 'dead'},
+      {2, 1} => %Cell{status: 'dead'},
+      {2, 2} => %Cell{status: 'alive'},
+      {2, 3} => %Cell{status: 'alive'},
+      {3, 0} => %Cell{status: 'alive'},
+      {3, 1} => %Cell{status: 'dead'},
+      {3, 2} => %Cell{status: 'alive'},
+      {3, 3} => %Cell{status: 'dead'},
+    }
+  end
+
+  def world() do
+    state()
+    |> World.new()
+  end
+end
