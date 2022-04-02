79e129135888abcac4b09ec3ea95629af4284bf3
Mario Viapiano <mario.viapiano@wertfaktor.de>
2022-04-02 17:16:07 +0200

    Add Application to start WorldManager process

---
+++ lib/golex/application.ex
@@ -0,0 +1,19 @@
+defmodule Golex.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      {Golex.Boundary.WorldManager, [name: Golex.Boundary.WorldManager]}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Mastery.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
